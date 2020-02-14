3224e820c77c0db0567d9a8618f6c7b9adc9e0e8
José Carneiro <josemcarneiro@gmail.com>
2020-02-14 16:22:29 +0000

    Added express mongoose example

--- node-express-mongoose/app.js
+++ node-express-mongoose/app.js
@@ -3,13 +3,28 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const Recipe = require('./models/recipe');
-
 const express = require('express');
+const nodeSassMiddleware = require('node-sass-middleware');
+const path = require('path');
 
 const app = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'hbs');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+app.use(
+  nodeSassMiddleware({
+    src: path.join(__dirname, '/public'),
+    dest: path.join(__dirname, '/public'),
+    force: isDevelopment
+  })
+);
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
   Recipe.find()
@@ -23,25 +38,100 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/recipe/create', (req, res) => {
+  res.render('create');
+});
+
+app.post('/recipe/create', (req, res, next) => {
+  const data = {
+    title: req.body.title,
+    cuisine: req.body.cuisine
+  };
+
+  Recipe.create(data)
+    .then(recipe => {
+      const id = recipe._id;
+      res.redirect(`/recipe/${id}`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+app.post('/recipe/:id/delete', (req, res, next) => {
+  const id = req.params.id;
+
+  Recipe.findByIdAndDelete(id)
+    .then(recipe => {
+      console.log('Delete recipe', id);
+      res.redirect(`/`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+app.get('/recipe/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  Recipe.findById(id)
+    .then(recipe => {
+      if (!recipe) {
+        next(new Error('NOT_FOUND'));
+      } else {
+        const data = { recipe };
+        res.render('edit', data);
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+app.post('/recipe/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  const data = {
+    title: req.body.title,
+    cuisine: req.body.cuisine
+  };
+  Recipe.findByIdAndUpdate(id, data, { runValidators: true })
+    .then(() => {
+      res.redirect(`/recipe/${id}`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
 app.get('/recipe/:id', (req, res, next) => {
   const id = req.params.id;
   Recipe.findById(id)
     .then(recipe => {
       if (!recipe) {
-        next();
+        next(new Error('NOT_FOUND'));
       } else {
         const data = { recipe };
         res.render('single', data);
       }
     })
     .catch(error => {
-      console.log(error);
-      res.send('Error');
+      next(error);
     });
 });
 
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found');
+app.use((error, req, res, next) => {
+  console.log('Error was caught in catch all handler');
+  console.log(error);
+
+  let status;
+  switch (error.message) {
+    case 'NOT_FOUND':
+      status = 404;
+      break;
+    default:
+      status = 500;
+  }
+
+  res.status(status).render('error', { error });
 });
 
 const mongoose = require('mongoose');
--- node-express-mongoose/models/recipe.js
+++ node-express-mongoose/models/recipe.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title: String,
+  title: {
+    type: String,
+    required: true
+  },
   level: String,
   ingredients: [String],
   cuisine: String,
